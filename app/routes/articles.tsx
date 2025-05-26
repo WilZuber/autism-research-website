64ec182e4f88aaba13967310a428eda9c344b00d
Wil <wilson.a.zuber@gmail.com>
2025-05-26 16:37:16 -0700

    Fix margins on articles

--- app/routes/articles.tsx
+++ app/routes/articles.tsx
@@ -24,7 +24,7 @@ export default function Home() {
           
         </div>
 
-        <div className="flex-col gap-1.5 flex max-w-3xl bg-white p-9 px-16">
+        <div className="flex-col gap-1.5 flex max-w-3xl bg-white p-9 px-16 w-3xl min-w-100">
           <h1 id='person-first' className='text-xl font-bold mt-4'><NavLink to="/language-around-autism" end className="hover:text-periwinkle">Language Around Autism</NavLink></h1>
           <p className='text-md'>Wilson Zuber</p>
           <p> Autism research is prevalent. It is crucially important that we understand the various language surrounding autism and autistic individuals, and how that language impacts the autistic community.</p>
