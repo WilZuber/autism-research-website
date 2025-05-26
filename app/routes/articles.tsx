df0013e3990be62cc71afad57ac8f59ffeef8a0c
Wil <wilson.a.zuber@gmail.com>
2025-05-26 16:09:59 -0700

    Make navbar its own component

--- app/routes/articles.tsx
+++ app/routes/articles.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import { NavLink } from "react-router";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,23 +15,7 @@ export default function Home() {
     <div className="flex flex-col">
 
 
-      <div className="flex-1 flex-col sticky top-0 w-screen bg-white">  {/* header */}
-
-        <div className="flex flex-col">
-          <nav className="pr-6 py-3 pl-4">
-            <div className="flex flex-row">
-              <nav className="w-14 flex-4"><h1 className="text-2xl text-periwinkle"><NavLink to="/" end className="hover:text-lightperiwinkle">Autism Language</NavLink></h1></nav>
-              <div className="flex-1 text-xl text-right"><NavLink to="/" end className="hover:text-periwinkle">Articles</NavLink></div>
-              <div className="flex-1 text-xl text-right"><NavLink to="/" end className="hover:text-periwinkle"> Resources</NavLink></div>
-              <div className="flex-1 text-xl text-right"><NavLink to="/" end className="hover:text-periwinkle">About</NavLink></div>
-              <div className="flex-1 text-xl text-right"><NavLink to="/" end className="hover:text-periwinkle">Contact Us</NavLink></div>
-            </div>
-          </nav>
-          <nav className="flex-8 p-4 pl-6 bg-lightperiwinkle">
-            <h1 className="text-white text-2xl text-center">Articles</h1>
-          </nav>
-        </div>
-      </div>
+      <Navbar title="Articles"></Navbar>
 
 
       <div className="flex flex-row gap-5 bg-greyperiwinkle justify-center h-screen">
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import { NavLink } from "react-router";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,25 +14,7 @@ export default function Home() {
   return [
     <div className="flex flex-col">
 
-
-      <div className="flex-1 flex-col sticky top-0 w-screen bg-white justify-center">  {/* header */}
-
-        <div className="flex flex-col">
-          <nav className="pr-6 py-3 pl-4">
-            <div className="flex flex-row">
-              <nav className="w-14 flex-4"><h1 className="text-2xl text-periwinkle"><NavLink to="/" end className="hover:text-lightperiwinkle">Autism Language</NavLink></h1></nav>
-              <div className="flex-1 text-xl text-right"><NavLink to="/articles" end className="hover:text-periwinkle">Articles</NavLink></div>
-              <div className="flex-1 text-xl text-right"><NavLink to="/resources" end className="hover:text-periwinkle"> Resources</NavLink></div>
-              <div className="flex-1 text-xl text-right"><NavLink to="/about" end className="hover:text-periwinkle">About</NavLink></div>
-              <div className="flex-1 text-xl text-right"><NavLink to="/contact" end className="hover:text-periwinkle">Contact Us</NavLink></div>
-            </div>
-          </nav>
-          <nav className="flex-8 p-4 pl-6 bg-lightperiwinkle">
-            <h1 className="text-white text-2xl mx-20">Language Around Autism - A Guide by Wilson Zuber</h1>
-          </nav>
-        </div>
-      </div>
-
+      <Navbar title="Language Around Autism - A Guide by Wilson Zuber"></Navbar>
 
       <div className="flex flex-row gap-5 bg-greyperiwinkle justify-center h-screen">
         
